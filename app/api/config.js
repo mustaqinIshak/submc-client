73538544647efc9957d19f0d0a6610cc105ac6a5
mustaqinIshak <mustaqinishak@gmail.com>
2024-08-25 15:13:27 +0800

    change config api again

--- app/api/config.js
+++ app/api/config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.NEXT_LOCAL_BASE_API_URL
-    // baseURL: process.env.NEXT_LIVE_BASE_API_URL
+    // baseURL: process.env.NEXT_LOCAL_BASE_API_URL
+    baseURL: process.env.NEXT_LIVE_BASE_API_URL
 })
 
 export default instance
