6aed6eacf30bbf365ff90d1da988c01781d29652
mustaqinIshak <mustaqinishak@gmail.com>
2024-09-12 18:36:14 +0800

    change logo cms

--- app/login/page.js
+++ app/login/page.js
@@ -54,8 +54,8 @@ export default function Login() {
     return (
             <div className="center-login">
                 <div className="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <div className="flex items-center">
-                        <Image alt="cmm apparel" src={"/logo/logoblack.png"} width={100} height={100} />
+                    <div className="mb-3 flex flex-col items-center">
+                        <Image className="mb-3" alt="cmm apparel" src={"/logo/logoblack.png"} width={150} height={150} />
                         <span className="font-semibold" >Portal CMS</span>
                     </div>
                     {
