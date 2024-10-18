5b7b959504cd4b558fe92772337a2a47f1964a7c
mustaqinIshak <mustaqinishak@gmail.com>
2024-10-18 14:52:10 +0800

    percobaan terakhir

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-"use client";
+'use client'
 import './globals.css'
 import './globals.css'
-import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from "../components/navbar"
 
